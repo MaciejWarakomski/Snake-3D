9dd3c75aafaa66d25949edb81f57bbb17e158c43
Maciej Warakomski <Warakom247365@gmail.com>
2021-03-05 14:13:19 +0100

    Initial commit - snake move and fruit spawner

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    [SerializeField] int maxXPos = 11;
+    [SerializeField] int maxYPos = 8;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 2f;
+    [SerializeField] List<GameObject> fruits;
+    RaycastHit hitInfo;
+
+    void Start()
+    {
+        StartCoroutine(StartSpawningFruit());
+    }
+
+    public IEnumerator StartSpawningFruit()
+    {
+        yield return new WaitForSeconds
+            (Random.Range(minSpawnDelay, maxSpawnDelay));
+        SpawnFruit();
+    }
+
+    private void SpawnFruit()
+    {
+        float fruitPosX;
+        float fruitPosY;
+        do
+        {
+            fruitPosX = Random.Range(-maxXPos, maxXPos + 1);
+            fruitPosY = Random.Range(-maxYPos, maxYPos + 1);
+
+            Physics.Raycast(
+                new Vector3(fruitPosX, 2f, fruitPosY),
+                Vector3.down,
+                out hitInfo);
+
+        } while (hitInfo.collider.transform.tag.Equals("Background"));
+
+        if(fruits.Count == 0)
+        {
+            Debug.LogError("Add fruits to list in Fruit Spawner!");
+            return;
+        }
+
+        Instantiate(fruits[Random.Range(0, fruits.Count)],
+            new Vector3(fruitPosX, 0f, fruitPosY),
+            Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float moveTimePeriod = 1f;
+    [SerializeField] Vector2Int startingPosition = new Vector2Int(0, 0);
+    Vector2Int snakePos;
+    Quaternion snakeRotation;
+    float moveTimer = 0f;
+
+    private void Awake()
+    {
+        snakePos = startingPosition;
+        transform.position = new Vector3(snakePos.x, 0, snakePos.y);
+    }
+
+    void Update()
+    {
+        ProcessDirection();
+        ProcessMove();
+
+    }
+
+    private void ProcessDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+        }
+    }
+
+    private void ProcessMove()
+    {
+        moveTimer += Time.deltaTime;
+        if (moveTimer >= moveTimePeriod)
+        {
+            transform.Translate(Vector3.forward);
+            moveTimer = 0f;
+        }
+    }
+}
